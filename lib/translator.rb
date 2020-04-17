c62319edcc45572055dc05c5fbd1f2a8acb06292
Khanittha Kraj <kkraj.swe@gmail.com>
2020-04-17 23:22:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,8 +20,8 @@ def load_library(file)
 end
 
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file, )
+  emoticon_hash = load_library("./lib/emoticons.yml")
 end
 
 def get_english_meaning
