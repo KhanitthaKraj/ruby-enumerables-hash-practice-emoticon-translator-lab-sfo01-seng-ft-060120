416ae25c901c80580b92b16e6e9dae6b678610a1
Khanittha Kraj <kkraj.swe@gmail.com>
2020-04-18 00:16:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -48,7 +48,7 @@ def get_japanese_emoticon(file, english_emoticon)
     flatten_value = value.flatten
     flatten_value.each do |emoticon|
       if english_emoticon == emoticon[1]
-        japanese_version = emoticon[1]
+        japanese_version = emoticon[3]
       end
        binding.pry
     end
