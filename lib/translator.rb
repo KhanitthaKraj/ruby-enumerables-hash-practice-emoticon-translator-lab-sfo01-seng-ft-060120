7c31e1cc028414e675237fabbdc66a2c426f2f23
Khanittha Kraj <kkraj.swe@gmail.com>
2020-04-18 00:16:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -50,6 +50,7 @@ def get_japanese_emoticon(file, english_emoticon)
       if english_emoticon == emoticon[1]
         japanese_version = emoticon[3]
       end
+      binding.pry
     end
   end
   japanese_version
